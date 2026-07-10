06064ff4f8a5e759c2dd686908bca2913ac77e86
Rudra Patidar <rudrapatidar24@gmail.com>
2026-07-10 23:38:40 +0530

    Time: 13 ms (92.63%), Space: 109 MB (5.17%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
