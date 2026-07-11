9e1a4189b3c10a451e75598511f61ca36ce8e839
Rudra Patidar <rudrapatidar24@gmail.com>
2026-07-11 22:03:18 +0530

    Time: 0 ms (100%), Space: 44.5 MB (82.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,9 +2,10 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int start=0;
         int end=nums.length-1;
-        int ans=0;
+        
         while(start<=end){
             int mid=(start+(end-start)/2);
+
             if(nums[0]>target){
             return 0;
             }
@@ -15,11 +16,9 @@ class Solution {
 
             if(nums[mid]<target){
                 start=mid+1;
-                ans=start;
             }
             else{
                 end=mid-1;
-                ans=end;
             }
         }
 
