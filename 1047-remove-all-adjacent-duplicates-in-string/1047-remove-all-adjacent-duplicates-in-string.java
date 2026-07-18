cb132da21fcc842a5192d2196dd1d1b2d2ab1bae
Rudra Patidar <rudrapatidar24@gmail.com>
2026-07-18 21:59:06 +0530

    Time: 12 ms (83.12%), Space: 46.7 MB (87%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        int n=s.length();
+        StringBuilder ans=new StringBuilder();
+        for(int i = 0 ; i<n;i++){
+            char ch=s.charAt(i);
+
+            if((ans.length())>0 && ans.charAt(ans.length()-1)==ch){
+                ans.deleteCharAt(ans.length()-1);
+            }
+            else{
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+
+    }
+}
